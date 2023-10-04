674da18f586d9daa83682bf31a31075d7884635d
arthdarji352 <arthdarji20@gmail.com>
2023-10-04 12:57:57 +0530

    sign In page functionality done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Signin from "./pages/SignIn";
+import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
