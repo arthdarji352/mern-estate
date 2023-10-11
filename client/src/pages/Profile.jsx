b09b8a46e22acc7d823a3a7ab93ee4d43bb9eaad
arthdarji352 <arthdarji20@gmail.com>
2023-10-11 19:24:43 +0530

    update listing functionality Done FE

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -158,6 +158,7 @@ const Profile = () => {
       console.log(error);
     }
   };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -277,7 +278,10 @@ const Profile = () => {
                 >
                   Delete
                 </button>
-                <button className="text-green-700 uppercase">Edit</button>
+
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
             </div>
           ))}
